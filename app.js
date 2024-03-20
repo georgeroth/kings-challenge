a31b44d777743a13a73ba1ab2f7fdb04e8ac3d8c
George Roth <george.roth@mastercard.com>
2024-03-20 11:59:36 +0000

    Test commit due to ssl unsafe legacy renegotiation

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 // Got to fire it up with NODE_TLS_REJECT_UNAUTHORIZED='0' due to MC policies
 
 fetch('https://gist.githubusercontent.com/christianpanton/10d65ccef9f29de3acd49d97ed423736/raw/b09563bc0c4b318132c7a738e679d4f984ef0048/kings')
- .then(function (response) {
-   return response.json()
- })
- .then(function (kingsData) {
+  .then(function (response) {
+    return response.json()
+  })
+  
+  .then(function (kingsData) {
     console.log(kingsData)
+
+    // 1. How many monarchs are there in the list?
+  
 })
