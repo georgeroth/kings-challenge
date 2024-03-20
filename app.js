5307d61913359e9cf3263a9afebcc59a6f85b636
George Roth <g@georgeroth.eu>
2024-03-20 16:47:46 +0000

    Exercise #3 added

--- app.js
+++ app.js
@@ -9,10 +9,10 @@ fetch('https://gist.githubusercontent.com/christianpanton/10d65ccef9f29de3acd49d
 
     // 1. How many monarchs are there in the list?
 
-      let monarchNameList = []
+      let monarchNameList = [] // A monarch might sneakily come back to rule again after they stepped down, so let's hold them in an array to make sure we don't count them twice
 
       monarchData.forEach(monarch => {
-        if (!monarchNameList.includes(monarch.nm)) { // A monarch might sneakily come back to rule again after they stepped down, so let's make sure we don't count them twice
+        if (!monarchNameList.includes(monarch.nm)) { 
             monarchNameList.push(monarch.nm)
           }
       })
@@ -46,4 +46,43 @@ fetch('https://gist.githubusercontent.com/christianpanton/10d65ccef9f29de3acd49d
       })
 
       console.log(`The longest ruling Monarch was (or were if there is a tie) ${longestRulingMonarchNameList}. They ruled for ${longestRulingMonarchLength} years.`)
+
+    // 3. Which house ruled the longest (and for how long)?
+
+      let housesAndLengths = []
+      let longestRulingHouseName = ""
+      let longestRulingHouseLength = 0
+
+      monarchData.forEach(monarch => {
+        const rulingYears = monarch.yrs.split("-")
+        let rulingLength = 0
+
+        if (rulingYears.length == 1) { // If someone has only a starting date it means they only ruled for 1 year
+          rulingLength = 1
+        } else if (rulingYears[1] == "") { // If someone has an empty ending date it means they are currently ruling, let's calculate from current year
+          const d = new Date();
+          rulingLength = d.getFullYear()-rulingYears[0]
+        } else { // Otherwise just calculate ruling years by subtracting rule start from rule end
+          rulingLength = rulingYears[1]-rulingYears[0]
+        }
+
+        const singleHouseAndLength = { // Preparing a single object to add to the array
+          nm: monarch.hse,
+          yrs: rulingLength
+        }
+      
+        if (!housesAndLengths.find((house) => house.nm === monarch.hse)) { // If there is no object in our array with that house name, we add the whole thing
+          housesAndLengths.push(singleHouseAndLength)
+        } else { // If there is already house name with the array, we only add the rulingLength
+          housesAndLengths.find((house) => house.nm === monarch.hse).yrs += rulingLength
+        }
+      })
+      
+      housesAndLengths.forEach(house => {
+        if (house.yrs > longestRulingHouseLength) { // If the current house is longer than the one we have, add their name and length to the variables
+          longestRulingHouseLength = house.yrs
+          longestRulingHouseName = house.nm
+        }
+      })
+      console.log(`The longest ruling house is ${longestRulingHouseName}. They ruled for a whopping ${longestRulingHouseLength} years.`)
 })
